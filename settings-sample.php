c25d8060ed4022d160f2e0961a4e6343cf8667b6
Zbynek Vyboh <zbynek@vyboh.net>
2019-09-18 17:38:17 +0200

    Create settings-sample.php

---
+++ settings-sample.php
@@ -0,0 +1,17 @@
+<?php
+/* Description:
+   Database settings 
+*/
+// The name of the database 
+define( 'DB_NAME', 'some_databases' );
+// username for DB
+define( 'DB_USER', 'some_user' );
+// password for DB
+define( 'DB_PASSWORD', 'some_password' );
+// MySQL hostname */
+define( 'DB_HOST', 'some_server.some_domain.tld' );
+// Database Charset
+define( 'DB_CHARSET', 'utf8' );
+// Database Collate type
+define( 'DB_COLLATE', '' );
+?>
