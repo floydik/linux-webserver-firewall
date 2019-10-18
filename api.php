8b20bb57e5b100bf66ad1fa2b20bd386dde1a14f
Zbynek Vyboh <zbynek@vyboh.net>
2019-10-18 11:53:13 +0200

    Create api.php
    Basic ideas only

---
+++ api.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Main script for insert rules
+*/
+
+// autentization
+// IP or range of IPs
+// user and pasword
+// api key
+// client certificate
+
+// verify IP (IPv4 or IPv6 or sth else)
+
+// IPv4 
+// Look up for IP. Is whitelisted? If yes exit
+// Look up for IP range. Is whitelisted? If yes exit
+// If is new then set semaphore = 2 and insert IP
+// if is exists then semaphore++ and update tables
+
+// IPv6
+// Look up for IP. Is whitelisted? If yes exit
+// Look up for IP range. Is whitelisted? If yes exit
+// If is new then set semaphore = 2 and insert IP
+// if is exists then semaphore++ and update tables
+
+
+?>
