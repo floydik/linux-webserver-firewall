aebd69f96822affedfc0513439c8ce46aec4f322
Zbynek Vyboh <zbynek@vyboh.net>
2019-10-18 12:10:25 +0200

    Update api.php

--- api.php
+++ api.php
@@ -10,6 +10,20 @@ Main script for insert rules
 // client certificate
 
 // verify IP (IPv4 or IPv6 or sth else)
+function filterinputip($ip) {
+    if (filter_var($ip, FILTER_VALIDATE_IP,FILTER_FLAG_IPV4)) {
+        $ex = 4;
+        }
+        elseif (filter_var($ip, FILTER_VALIDATE_IP,FILTER_FLAG_IPV6)) {
+          $ex = 6;
+        }
+    else {
+        $ex = 0;
+        }
+  return($ex);
+}
+
+
 
 // IPv4 
 // Look up for IP. Is whitelisted? If yes exit
@@ -23,5 +37,14 @@ Main script for insert rules
 // If is new then set semaphore = 2 and insert IP
 // if is exists then semaphore++ and update tables
 
+$x = "217.198.116.129";
+$i = filterinputip($x);
+echo $i.PHP_EOL;
+$x = "2a00:19a0:3:74:0:d9c6:7481:1";
+$i = filterinputip($x);
+echo $i.PHP_EOL;
+$x = "test.rest.cz";
+$i = filterinputip($x);
+echo $i.PHP_EOL;
 
 ?>
