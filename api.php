55bed0ca52d62c7b77468016be967d3156d78a25
Zbynek Vyboh <zbynek@vyboh.net>
2020-06-30 14:12:23 +0200

    Update api.php

--- api.php
+++ api.php
@@ -28,16 +28,36 @@ function filterinputip($ip) {
 // IPv4 
 function insertipv4($ip) {
 // Look up for IP. Is whitelisted? If yes exit
-    $q = "SELECT * FROM `ipv4` WHERE `ip` LIKE $ip;";
+    $q = "SELECT * FROM `ipv4` WHERE `ip` LIKE $ip AND `semaphore_id` = 0;";
 // Look up for IP range. Is whitelisted? If yes exit
-// If is new then set semaphore = 2 and insert IP
+// If is new then set semaphore = 3 and insert IP
 // if is exists then semaphore++ and update tables
-}
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        if (mysqli_connect_error()) {
+        die('Connect Error (' . mysqli_connect_errno() . ') '
+           . mysqli_connect_error());
+        }
+    //
+    if ($result = $mysqli->query($q)) {
+            if ($result->num_rows) > 0 {
+                $result->close();
+                exit();
+            }
+    } else
+        {
+            $q = "INSERT into `ipv4` (`id`, `ip`, `mask`, `updatetime`, `semaphore_id`) VALUES (NULL, '$ip', '32', CURRENT_TIME(), '3');";
+            echo $q;
+            if ($mysqli->query($q) === TRUE) {
+                printf("OK\n");
+            }
+        }
+    $mysqli->close();
+} // end of insertipv4
 
 // IPv6
 // Look up for IP. Is whitelisted? If yes exit
 // Look up for IP range. Is whitelisted? If yes exit
-// If is new then set semaphore = 2 and insert IP
+// If is new then set semaphore = 3 and insert IP
 // if is exists then semaphore++ and update tables
 
 $x = "217.198.116.129";
