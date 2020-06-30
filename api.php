10a4672be119d041b906594fc6579cda04f2d978
Zbynek Vyboh <zbynek@vyboh.net>
2020-06-30 14:31:56 +0200

    Update api.php

--- api.php
+++ api.php
@@ -42,6 +42,7 @@ function insertipv4($ip) {
         }
     //
     if ($result = $mysqli->query($q)) {
+        echo "a sem se nikdy nedostanu".PHP_EOL;
             if (($result->num_rows) == 0) {
                 $resut->close();
                 $q = "INSERT into `ipv4` (`id`, `ip`, `mask`, `updatetime`, `semaphore_id`) VALUES (NULL, '$ip', '32', NULL, '3');";
