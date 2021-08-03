bcc78b408170bcb7910cc7bbe99639d5f6660332
Zbynek Vyboh <zbynek@vyboh.net>
2021-08-03 15:13:40 +0200

    Create mutliple-attack-detect.php

---
+++ mutliple-attack-detect.php
@@ -0,0 +1,34 @@
+<?php
+// some settings
+require("settings.php");
+
+class Rules
+{
+    function getrules();
+    {
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        if (mysqli_connect_error()) {
+              die('Connect Error (' . mysqli_connect_errno() . ') '
+           .  mysqli_connect_error());
+        }
+        echo 'Success... ' . $mysqli->host_info . "\n";
+        if ($stmt = $mysqli->prepare("SELECT `regex` , `log` , `threshold` , `execute` FROM `rules` WHERE `active` = 1;")) {
+                $stmt->execute();
+                $stmt->bind_result($rgx,$log,$trh,$ex);
+                while ($stmt->fetch()) {
+                        $execute = "";
+                        echo $execute.PHP_EOL;
+                        $out =  shell_exec($execute);
+                }
+                $stmt->close();
+        }
+
+      
+
+    } // getrules
+  
+// SELECT `regex` , `log` , `threshold` , `execute` FROM `rules` WHERE `active` = 1;
+
+
+} // end of Rules
+?>
